fcc82b71ea0baa8cb188ac39e94a9a6429df7914
Jason Snider <root@jasonsnider>
2018-01-26 10:57:14 -0600

    Implemented a user view

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -23,4 +23,7 @@ export class UserService {
     return this.http.get<User>(this.url);
   }
 
+  getUser(id: string): Observable<User> {
+    return this.http.get<User>(this.url + `/view/${id}`);
+  }
 }
